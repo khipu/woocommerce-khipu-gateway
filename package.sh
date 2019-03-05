9ae9df59edd15b08f0d09e1199b66b01e24836e5
chaotive <leonardomontenegro123@gmail.com>
2019-03-05 11:29:32 -0300

    Fixing package script

--- package.sh
+++ package.sh
@@ -1,6 +1,19 @@
+#!/usr/bin/env bash
+
 cd ..
-rm -rf woocommerce-khipu-gateway woocommerce-khipu-gateway.zip
-cp -R woocommerce-khipu woocommerce-khipu-gateway
-rm -rf woocommerce-khipu-gateway/.git woocommerce-khipu-gateway/.gitignore woocommerce-khipu-gateway/.gitmodules woocommerce-khipu-gateway/.idea woocommerce-khipu-gateway/.DS_Store woocommerce-khipu-gateway/package.sh woocommerce-khipu-gateway/lib/lib-khipu/.git woocommerce-khipu-gateway/lib/lib-khipu/.gitignore woocommerce-khipu-gateway/lib/lib-khipu/.gitattributes
-zip -r woocommerce-khipu-gateway.zip woocommerce-khipu-gateway
-rm -rf woocommerce-khipu-gateway
+rm -rf woocommerce-khipu-gateway.zip
+cp -R woocommerce-khipu-gateway woocommerce-khipu-gateway-release
+rm -rf \
+    woocommerce-khipu-gateway-release/.git \
+    woocommerce-khipu-gateway-release/.gitignore \
+    woocommerce-khipu-gateway-release/.gitmodules \
+    woocommerce-khipu-gateway-release/.idea \
+    woocommerce-khipu-gateway-release/.DS_Store \
+    woocommerce-khipu-gateway-release/composer.phar \
+    woocommerce-khipu-gateway-release/package.sh \
+    woocommerce-khipu-gateway-release/lib/lib-khipu/.git \
+    woocommerce-khipu-gateway-release/lib/lib-khipu/.gitignore \
+    woocommerce-khipu-gateway-release/lib/lib-khipu/.gitattributes
+zip -r woocommerce-khipu-gateway.zip woocommerce-khipu-gateway-release
+rm -rf woocommerce-khipu-gateway-release
+cd woocommerce-khipu-gateway
